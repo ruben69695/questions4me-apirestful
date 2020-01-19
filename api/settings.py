31449175c25d003e5c34e939dc36fb8b1bf29e5b
Ruben Arrebola <ruben.arre6@gmail.com>
2020-01-19 23:21:35 +0100

    Settings changed

--- api/settings.py
+++ api/settings.py
@@ -164,12 +164,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_URL = '/staticfiles/'
 
 
 CORS_ORIGIN_WHITELIST = [
