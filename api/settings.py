464f7bded78c6c4c226bab32e8db5c5d7f44bfca
Ruben Arrebola <ruben.arre6@gmail.com>
2020-01-19 23:05:52 +0100

    django heroku support for deployment

--- api/settings.py
+++ api/settings.py
@@ -171,3 +171,8 @@ CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
     "http://127.0.0.1:3000"
 ]
+
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
